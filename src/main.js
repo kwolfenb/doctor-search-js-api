016a699b8ae01d28a73189ad72c4b8ff412c5c26
Kenny Wolfenberger <kennywolfenberger@gmail.com>
2019-01-18 14:21:23 -0800

    updates with table results, api failure error message

--- src/main.js
+++ src/main.js
@@ -7,41 +7,59 @@ import { Doctor } from './doctor.js';
 $(document).ready(function () {
   $("#search").submit(function (event) {
     event.preventDefault();
-    $(".result").show();
-    
+
     let searchTerm = $("#searchTerm").val();
     let name = $("#name").val();
     let doctor = new Doctor(searchTerm, name);
-
+    let acceptsPatients = "No";
     $("#showDetails").text(`here are your results for ${doctor.searchTerm}`);
-    
+
     $.ajax({
       url: doctor.url,
       type: 'Get',
+      timeout: 1000,
       data: {
         format: 'json'
       },
       success: function (response) {
-        for (let i = 0; i < response.data.length; i++) {
-          $("#table").append(`<tr id=${i}></tr>`);
+        $(".result").show();
+        let resultsCount = response.data.length;
+        if (resultsCount < 1) {
+          $("#resultCount").text(`Your search returned 0 results.`);
+          $("#table").hide();
         }
-        for (let i = 0; i < response.data.length; i++) {
-          $("#"+i).html(
-            `<td>${response.data[i].profile.first_name}</td>
-            <td>${response.data[i].profile.last_name}</td>
-            <td>${response.data[i].practices[0].visit_address.street}, ${response.data[i].practices[0].visit_address.city}</td>
-            <td> ${response.data[i].practices[0].phones[0].number}</td>
-            <td>${response.data[i].practices[0].website}</td>
-            <td> ${response.data[i].practices[0].accepts_new_patients}</td>`);
+        else {
+          $("table tr:gt(0)").remove();
+          $("#table").show();
+          $("#resultCount").text(`Your search returned ${resultsCount} results.`);
+          for (let i = 0; i < resultsCount; i++) {
+            $("#table").append(`<tr id=${i}></tr>`);
+          }
+          for (let i = 0; i < resultsCount; i++) {
+            if (response.data[i].practices[0].accepts_new_patients == true) {
+              acceptsPatients = "Yes";
+            }
+            $("#" + i).html(
+              `<td>${response.data[i].profile.first_name}</td>
+              <td>${response.data[i].profile.last_name}</td>
+              <td>${response.data[i].practices[0].visit_address.street}, ${response.data[i].practices[0].visit_address.city}</td>
+              <td> ${response.data[i].practices[0].phones[0].number}</td>
+              <td>${response.data[i].practices[0].website}</td>
+              <td> ${acceptsPatients}</td>`);
+            acceptsPatients = "No";
+          }
         }
       },
-      error: function () {
-        $('#errors').text("There was an error processing your request. Please try again.");
+      error: function (xhr, status) {
+        let errorMessage = "There was a problem with this request. Status: " + status + ', Error Message: ' + xhr.statusText;
+        $('#errors').text(errorMessage);
       }
     });
 
   });
 
-
+  $("#reset").click(function () {
+    location.reload();
+  });
 
 });
