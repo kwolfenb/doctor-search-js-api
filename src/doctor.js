57e161ee31dffcf8ec07a6215ccab7dfc9eade08
Kenny Wolfenberger <kennywolfenberger@gmail.com>
2019-01-18 13:14:50 -0800

    finishes api build and displays results in ui html

--- src/doctor.js
+++ src/doctor.js
@@ -5,14 +5,15 @@ export class Doctor {
     this.searchTerm = searchTerm;
     this.apiKey = process.env.exports.apiKey;
 
-    this.url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&query=${searchTerm}&location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=da51cf8ea8fb58522b23571dc3e80390`;
+    this.url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&query=${searchTerm}&location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=${this.apiKey}`;
 
   }
 
 
-
-
-
 }
+
+
+
+
 
 
--- src/main.js
+++ src/main.js
@@ -4,30 +4,44 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Doctor } from './doctor.js';
 
-
-
-$(document).ready(function() {
-  $("#search").submit(function(event) {
+$(document).ready(function () {
+  $("#search").submit(function (event) {
     event.preventDefault();
+    $(".result").show();
+    
     let searchTerm = $("#searchTerm").val();
     let name = $("#name").val();
     let doctor = new Doctor(searchTerm, name);
-    console.log(doctor.url);
+
+    $("#showDetails").text(`here are your results for ${doctor.searchTerm}`);
+    
     $.ajax({
       url: doctor.url,
       type: 'Get',
       data: {
         format: 'json'
       },
-      success: function(response) {
-        $("#showDetails").text(`here are your results for ${searchTerm}`);
-        $("#description").text(`${response.data.practices.name}`);
-      }, 
-      error: function() {
+      success: function (response) {
+        for (let i = 0; i < response.data.length; i++) {
+          $("#table").append(`<tr id=${i}></tr>`);
+        }
+        for (let i = 0; i < response.data.length; i++) {
+          $("#"+i).html(
+            `<td>${response.data[i].profile.first_name}</td>
+            <td>${response.data[i].profile.last_name}</td>
+            <td>${response.data[i].practices[0].visit_address.street}, ${response.data[i].practices[0].visit_address.city}</td>
+            <td> ${response.data[i].practices[0].phones[0].number}</td>
+            <td>${response.data[i].practices[0].website}</td>
+            <td> ${response.data[i].practices[0].accepts_new_patients}</td>`);
+        }
+      },
+      error: function () {
         $('#errors').text("There was an error processing your request. Please try again.");
       }
     });
+
   });
-  
+
+
 
 });
