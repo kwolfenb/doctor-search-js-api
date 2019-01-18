d29b0f26924a31911dc9be81b1b15d5550dc61f4
Kenny Wolfenberger <kennywolfenberger@gmail.com>
2019-01-18 15:09:51 -0800

    adds promse functionality to api request

--- src/main.js
+++ src/main.js
@@ -12,54 +12,45 @@ $(document).ready(function () {
     let name = $("#name").val();
     let doctor = new Doctor(searchTerm, name);
     let acceptsPatients = "No";
-    $("#showDetails").text(`here are your results for ${doctor.searchTerm}`);
 
-    $.ajax({
-      url: doctor.url,
-      type: 'Get',
-      timeout: 1000,
-      data: {
-        format: 'json'
-      },
-      success: function (response) {
-        $(".result").show();
-        let resultsCount = response.data.length;
-        if (resultsCount < 1) {
-          $("#resultCount").text(`Your search returned 0 results.`);
-          $("#table").hide();
-        }
-        else {
-          $("table tr:gt(0)").remove();
-          $("#table").show();
-          $("#resultCount").text(`Your search returned ${resultsCount} results.`);
-          for (let i = 0; i < resultsCount; i++) {
-            $("#table").append(`<tr id=${i}></tr>`);
-          }
-          for (let i = 0; i < resultsCount; i++) {
-            if (response.data[i].practices[0].accepts_new_patients == true) {
-              acceptsPatients = "Yes";
-            }
-            $("#" + i).html(
-              `<td>${response.data[i].profile.first_name}</td>
-              <td>${response.data[i].profile.last_name}</td>
-              <td>${response.data[i].practices[0].visit_address.street}, ${response.data[i].practices[0].visit_address.city}</td>
-              <td> ${response.data[i].practices[0].phones[0].number}</td>
-              <td>${response.data[i].practices[0].website}</td>
-              <td> ${acceptsPatients}</td>`);
-            acceptsPatients = "No";
-          }
-        }
-      },
-      error: function (xhr, status) {
-        let errorMessage = "There was a problem with this request. Status: " + status + ', Error Message: ' + xhr.statusText;
-        $('#errors').text(errorMessage);
+    let promise = doctor.searchFunction();
+    promise.then(function (response) {
+      $(".result").show();
+      let body = JSON.parse(response);
+      let resultsCount = body.data.length;
+      if (resultsCount < 1) {
+        $("#resultCount").text(`Your search returned 0 results.`);
+        $("#table").hide();
       }
+      else {
+        $("table tr:gt(0)").remove();
+        $("#table").show();
+        $("#resultCount").text(`Your search returned ${resultsCount} results.`);
+        for (let i = 0; i < resultsCount; i++) {
+          $("#table").append(`<tr id=${i}></tr>`);
+        }
+        for (let i = 0; i < resultsCount; i++) {
+          if (body.data[i].practices[0].accepts_new_patients == true) {
+            acceptsPatients = "Yes";
+          }
+          $("#" + i).html(
+            `<td>${body.data[i].profile.first_name}</td>
+              <td>${body.data[i].profile.last_name}</td>
+              <td>${body.data[i].practices[0].visit_address.street}, ${body.data[i].practices[0].visit_address.city}</td>
+              <td> ${body.data[i].practices[0].phones[0].number}</td>
+              <td><a href="${body.data[i].practices[0].website}">${body.data[i].practices[0].website}</a></td>
+              <td> ${acceptsPatients}</td>`);
+          acceptsPatients = "No";
+        }
+      }
+    }, function (err) {
+      $('#errors').text(`There was an error processing your request: ${err.message}`);
     });
 
-  });
+    $("#reset").click(function () {
+      location.reload();
+    });
 
-  $("#reset").click(function () {
-    location.reload();
   });
 
 });
